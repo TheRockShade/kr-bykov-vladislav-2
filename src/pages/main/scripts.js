c7061082d698b4e254d3a338d61b4d3be761bd38
Vladislav Bykov <therockshade@gmail.com>
2020-08-31 06:09:42 +0400

    feat: add mobile menu

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,3 +1,5 @@
+// Popup
+
 let modalOpen = document.querySelector('.button--slider'),
 		modalClose = document.querySelector('.close'),
 		popup = document.querySelector('.popup-bg'),
@@ -13,8 +15,37 @@ modalClose.addEventListener('click', function() {
 	modalOpen.focus();
 });
 
+// Menu
+
+let menuOpen = document.querySelector('.menu-open'),
+		menuClose = document.querySelector('.menu-close'),
+		menu = document.querySelector('.menu'),
+		header = document.querySelector('.header'),
+		menuFocus = document.querySelector('.item_focus');
+
+menuOpen.addEventListener('click', function() {
+	header.classList.add('header_sticky');
+	menu.classList.add('menu_active');
+	menuOpen.classList.add('menu-open_disable');
+	menuClose.classList.add('menu-close_active');
+	menuFocus.focus();
+});
+
+menuClose.addEventListener('click', function() {
+	header.classList.remove('header_sticky');
+	menu.classList.remove('menu_active');
+	menuOpen.classList.remove('menu-open_disable');
+	menuClose.classList.remove('menu-close_active');
+	menuOpen.focus();
+});
+
 window.addEventListener('keydown', function(e) {
 	if (e.code === 'Escape' && popup.classList.contains('popup_open')) {
 		popup.classList.remove('popup_open');
+	};
+	if (e.code === 'Escape' && menu.classList.contains('menu_active')) {
+		menu.classList.remove('menu_active');
+		menuOpen.classList.remove('menu-open_disable');
+		menuClose.classList.remove('menu-close_active');
 	};
 });
